b9c79b087eafd18a3568c6eb46388fd2798de0ec
Prasad Solase <prasadsolase1234@gmail.com>
2026-09-19 23:42:28 +0530

    Initial Project Foresight dashboard

--- pages/4_Risk.py
+++ pages/4_Risk.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
- 
+  
 # PAGE CONFIGURATION
 st.set_page_config(
     page_title="Foresight - Risk Dashboard",

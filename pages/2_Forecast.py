29620c2303e65ebfdd6d6106f46e5588751e73d2
Prasad Solase <prasadsolase1234@gmail.com>
2026-09-20 03:41:42 +0530

    Update 2_Forecast.py

--- pages/2_Forecast.py
+++ pages/2_Forecast.py
@@ -36,7 +36,7 @@ sales_path = os.path.join(
 @st.cache_data
 def load_sales_data():
 
-    sales = pd.read_csv(sales_path)
+    sales = pd.read_excel(sales_path, engine="xlrd")
 
     sales.columns = sales.columns.str.strip()
 
